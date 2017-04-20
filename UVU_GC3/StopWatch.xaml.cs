f9adc6c2fb7d1bea14c09a6cce1b0d71aa6cac2c
Jimmy LoForti <jamesloforti@gmail.com>
2017-04-19 22:07:34 -0600

    StopWatch: functionality at 90%

--- UVU_GC3/StopWatch.xaml.cs
+++ UVU_GC3/StopWatch.xaml.cs
@@ -20,16 +20,14 @@ namespace UVU_GC3
     public sealed partial class StopWatch : UserControl
     {
         //Declare Class Vars:
-        //private static int ticked = 1;
-        //private static int toTick = 59;
-        private static string _defaultTime = "00:00:00";
-
         private DispatcherTimer _timer;
         private DateTime _startTime = DateTime.MinValue;
         public TimeSpan _currentElapsed = TimeSpan.Zero;
         public TimeSpan _totalElapsed = TimeSpan.Zero;
-        private bool _timerRunning = false;
         private static TimeSpan _timeSinceStart;
+        //private static TimeSpan _manipulatedTime;
+        private bool _timerRunning = false;
+        private static string _defaultTime = "00:00:00";
 
         /// <summary>
         /// Default Constructor - To initialize components
@@ -70,13 +68,13 @@ namespace UVU_GC3
         /// </summary>
         private void ProcessTick()
         {
-            //
+            //Calc difference between now and start time
             _timeSinceStart = DateTime.Now - _startTime;
             _timeSinceStart = new TimeSpan(_timeSinceStart.Hours,
                                             _timeSinceStart.Minutes,
                                             _timeSinceStart.Seconds);
 
-            //
+            //Set currentElapsed to sum of timeSinceStart and totalElapsed
             _currentElapsed = _timeSinceStart + _totalElapsed;
             TimeTxt.Text = _currentElapsed.ToString();
         } // end method ProcessTick()
@@ -135,75 +133,68 @@ namespace UVU_GC3
         } // end method RestartBtn_Click()
 
         /// <summary>
-        /// SeekBckBtn_Click - To repeatedly decrement timer tick and update displayed time
+        /// SeekBckBtn_Click - To repeatedly decrement total elapsed time and update displayed time
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void SeekBckBtn_Click(object sender, RoutedEventArgs e)
         {
-            //
-            _timer.Stop();
-            _timerRunning = false;
-
             //If timer is greater than zero
             if (_currentElapsed.Seconds > 0)
             {
-                //_currentElapsed = _currentElapsed.Add(DateTime.Now.AddSeconds(1).To);
+                //Stop timer and set flag
+                _timer.Stop();
+                _timerRunning = false;
 
+                //Create new TimeSpan using currently displayed time
+                TimeSpan currDisplayed = new TimeSpan(int.Parse(TimeTxt.Text.Substring(0, 2)),
+                                                        int.Parse(TimeTxt.Text.Substring(3, 2)),
+                                                        int.Parse(TimeTxt.Text.Substring(6, 2)));
 
-                //
-                _timeSinceStart = DateTime.Now.AddSeconds(3) - _startTime;
-                //timeSinceStart = new TimeSpan(timeSinceStart.Hours,
-                //                                timeSinceStart.Minutes,
-                //                                timeSinceStart.Seconds);
+                //Subtract 10 seconds to currently displayed time and display the new time
+                currDisplayed = currDisplayed.Subtract(new TimeSpan(0, 0, 10));
+                TimeTxt.Text = currDisplayed.ToString();
 
-                //
-                _currentElapsed = _timeSinceStart + _totalElapsed;
-                TimeTxt.Text = _currentElapsed.ToString();
+                //Add fast forward changes to main TimeSpan mechanism
+                _totalElapsed = new TimeSpan(currDisplayed.Hours, currDisplayed.Minutes, currDisplayed.Seconds);
+                _currentElapsed = _totalElapsed;
 
+                //Start timer and set flag
                 _timer.Start();
+                _timerRunning = true;
             } // end if
         } // end method SeekBckBtn_Click()
 
         /// <summary>
-        /// SeekFwdBtn_Click - To repeatedly increment timer tick and update displayed time
+        /// SeekFwdBtn_Click - To repeatedly increment total elapsed time and update displayed time
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void SeekFwdBtn_Click(object sender, RoutedEventArgs e)
         {
-            //
+            //Stop timer and set flag
             _timer.Stop();
             _timerRunning = false;
 
-            TimeSpan currDisplayed = new TimeSpan(int.Parse(TimeTxt.Text.Substring(0, 2)), int.Parse(TimeTxt.Text.Substring(3, 2)), int.Parse(TimeTxt.Text.Substring(6, 2)));
+            //Create new TimeSpan using currently displayed time
+            TimeSpan currDisplayed = new TimeSpan(int.Parse(TimeTxt.Text.Substring(0, 2)), 
+                                                    int.Parse(TimeTxt.Text.Substring(3, 2)), 
+                                                    int.Parse(TimeTxt.Text.Substring(6, 2)));
 
+            //Add 10 seconds to currently displayed time and display the new time
             currDisplayed = currDisplayed.Add(new TimeSpan(0, 0, 10));
-
             TimeTxt.Text = currDisplayed.ToString();
 
-            _timeSinceStart = new TimeSpan(currDisplayed.Hours, currDisplayed.Minutes, currDisplayed.Seconds);
+            //Add fast forward changes to main TimeSpan mechanism
+            //_timeSinceStart = new TimeSpan(currDisplayed.Hours, currDisplayed.Minutes, currDisplayed.Seconds);
+            _totalElapsed = new TimeSpan(currDisplayed.Hours, currDisplayed.Minutes, currDisplayed.Seconds);
 
-            _currentElapsed = _timeSinceStart + _totalElapsed;
+            //_currentElapsed = _timeSinceStart;// + _totalElapsed;
+            _currentElapsed = _totalElapsed;
 
-            //timeee.Add(new TimeSpan(0, 0, 1));
-
-            //timeSinceStart.Add(new TimeSpan(0, 0, 1)) - _startTime;
-            //timeSinceStart = DateTime.Now.AddMilliseconds(1000) - _startTime; 
-            //timeSinceStart = new TimeSpan(timeSinceStart.Hours,
-            //                                timeSinceStart.Minutes,
-            //                                timeSinceStart.Seconds);
-
-            //
-            //_currentElapsed = timeSinceStart + _totalElapsed;
-            //TimeTxt.Text = _currentElapsed.ToString();
-
-            //_timer.Start();
+            //Start timer and set flag
+            _timer.Start();
+            _timerRunning = true;
         } // end method SeekFwdBtn_Click()
-
-        private void SeekFwdBtn_PointerReleased(object sender, PointerRoutedEventArgs e)
-        {
-            //_timer.Start();
-        }
     } // end class StopWatch
 } // end namespace UVU_GC3
